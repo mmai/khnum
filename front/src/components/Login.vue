817f3de120d11818f18a414d8f71afce4c88e265
Henri Bourcereau <henri.bourcereau@gmail.com>
2019-05-20 21:14:27 +0200

    vuejs wip

---
+++ front/src/components/Login.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="login">
+    <h1>Email Invitation</h1>
+
+    
+    {{ log }}
+
+    <p>{{ msg }}</p>
+    <input v-model="email"><br>
+    <button v-on:click="sendVerificationEmail">Send Email</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: "Login",
+  props: {
+    msg: String,
+    log: String
+  },
+  methods: {
+    sendVerificationEmail: function () {
+      axios.post( 'api/invitation', {
+          email: this.email,
+      })
+      .then((response) => {
+        this.msg = "Please check your email.";
+        this.email = "";
+        this.log = response;
+      })
+      .catch((e) => {
+        this.log = e;
+      })
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+
+
+.login { 
+	width:600px;
+	margin:auto;
+	border:1px #CCC solid;
+	padding:0px 30px;
+	background-color: #3b6caf;
+	color:#FFF;
+}
+
+.field { 
+	background: #1e4f8a;
+	border:1px #03306b solid;
+	padding:10px;
+	margin:5px 25px;
+	width:215px;
+	color:#FFF;
+ }
+
+.login h1, p, .chbox, .btn {
+	margin-left:25px;
+	color:#fff;
+}
+
+.btn {
+	background-color: #00CCFF;
+	border:1px #03306b solid;
+	padding:10px 30px;
+	font-weight:bold;
+  margin:25px 25px;
+  cursor: pointer;
+}
+
+.forgot {
+	color:#fff;
+}
+</style>
---
+++ front/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Login msg="Please enter your email to receive an Invitation" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from "@/components/Login.vue";
+
+export default {
+  name: "home",
+  components: {
+    Login
+  }
+};
+</script>

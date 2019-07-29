6bbdada58733cfa0411977401ed31444dd086456
Henri Bourcereau <henri.bourcereau@gmail.com>
2019-07-29 18:57:29 +0200

    vue add vuetify

--- front/src/main.js
+++ front/src/main.js
@@ -2,14 +2,18 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+
   mounted() {
     store.dispatch("auth_check");
   },
+
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="home">
-    Welcome to Activue
-  </div>
+  <v-container>
+    <v-layout text-center wrap>
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Activue
+        </h1>
+        <p class="subheading font-weight-regular">
+          An example of using Actix with Vue (+ vuex & vuetify)
+        </p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: "home",
+  name: "home"
 };
 </script>

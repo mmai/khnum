c910d4be09be9c93435429afc7a423bcfaa03bf1
Henri Bourcereau <henri.bourcereau@gmail.com>
2019-08-05 12:47:03 +0200

    front: login form as a modal

--- front/src/components/Auth.vue
+++ front/src/components/Auth.vue
@@ -5,7 +5,50 @@
   </span>
   <span v-else>
     <v-btn text to="/invitation">Get invite</v-btn>
-    <v-btn text to="/login">Login</v-btn>
+    <v-dialog v-model="loginDialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn color="primary" v-on="on">Login</v-btn>
+      </template>
+
+      <v-card>
+        <v-toolbar color="primary" dark flat>
+          <v-toolbar-title>Login</v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              @focus="loginRemoveError()"
+              v-model="username"
+              label="Login"
+              name="login"
+              prepend-icon="person"
+              type="text"
+              :error="loginShowError"
+            ></v-text-field>
+
+            <v-text-field
+              @focus="loginRemoveError()"
+              v-model="password"
+              id="password"
+              label="Password"
+              name="password"
+              prepend-icon="lock"
+              type="password"
+              :error="loginShowError"
+            ></v-text-field>
+            <v-alert type="error" v-show="loginShowError">
+              {{ loginErrorMessage }}
+            </v-alert>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn @click="close" color="warning">Cancel</v-btn>
+          <v-btn @click="login" color="primary">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </span>
 </template>
 
@@ -13,9 +56,17 @@
 export default {
   name: "Auth",
   data() {
-    return {};
+    return {
+      loginDialog: false,
+      loginErrorMessage: "",
+      username: "",
+      password: ""
+    };
   },
   computed: {
+    loginShowError() {
+      return this.loginErrorMessage != "";
+    },
     fromConnected() {
       // return this.$store.state.connected;
       return this.$store.getters.isConnected;
@@ -28,8 +79,25 @@ export default {
     log: String
   },
   methods: {
+    close: function() {
+      this.loginDialog = false;
+    },
     logout: function() {
       this.$store.dispatch("logout").then(() => this.$router.push("/"));
+    },
+    login: function() {
+      this.$store
+        .dispatch("login", { username: this.username, password: this.password })
+        .then(() => {
+          this.loginDialog = false;
+          // this.$router.push("/");
+        })
+        .catch(err => {
+          this.loginErrorMessage = err.response.data;
+        });
+    },
+    loginRemoveError: function() {
+      this.loginErrorMessage = "";
     }
   }
 };
--- front/src/router.js
+++ front/src/router.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-import Login from "./views/Login.vue";
 
 Vue.use(Router);
 
@@ -13,11 +12,6 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
-    },
-    {
-      path: "/login",
-      name: "login",
-      component: Login
     },
     {
       path: "/invitation",

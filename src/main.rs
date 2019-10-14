f6bb4c0492316c54cf83f1ba961d2e97f37c5c3e
Henri Bourcereau <henri.bourcereau@gmail.com>
2019-10-14 12:34:26 +0200

    fix i18n

--- src/main.rs
+++ src/main.rs
@@ -24,11 +24,11 @@ use gettext_macros::{compile_i18n, include_i18n, init_i18n};
 
 use crate::khnum::wiring;
 
+init_i18n!("khnum", en, fr); // Put this before modules containing messages to be translated
 mod khnum;
 
 use crate::khnum::wiring::{DbPool, Config};
 
-init_i18n!("khnum", en, fr); // Put this before modules containing messages to be translated
 
 // fn hello(
 //     session: Session,
